791581c6d44ea4b5add1bc8429779eaaf19a85c8
Amitesh <rakshitamitesh@gmail.com>
2023-07-21 12:37:30 +0530

    done db Connection

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const { notFound, errorHandler } = require('./middlewares');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 1337;
+
+// Connection to the database
+
+mongoose.connect(process.env.MONGO_URL).then(
+  app.listen(port, () => {
+    console.log('Server started!');
+  }),
+);
 
 // Setting up the middlewares
-const app = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
 
-const port = process.env.PORT || 1337;
+app.use(notFound);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.json({
     message: 'You\'re at the homepage now',
   });
-});
-
-// Middlewares
-app.use(notFound);
-app.use(errorHandler);
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
 });
