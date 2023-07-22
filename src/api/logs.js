e6abf104af27ff34d5d27c05448f63aaf5c06c6d
Amitesh <rakshitamitesh@gmail.com>
2023-07-22 10:44:45 +0530

    basics routing done

--- src/api/logs.js
+++ src/api/logs.js
@@ -3,12 +3,17 @@ const LogEntry = require('../models/LogEntry');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: '🌏',
-  });
+// route for getting all the log entries
+router.get('/', async (req, res, next) => {
+  try {
+    const allLogs = await LogEntry.find();
+    res.json(allLogs);
+  } catch (error) {
+    next(error);
+  }
 });
 
+// creating a new log entry
 router.post('/', async (req, res, next) => {
   try {
     const logEntry = new LogEntry(req.body);
@@ -21,5 +26,16 @@ router.post('/', async (req, res, next) => {
     next(error);
   }
 });
+
+/*
+// updating a log entry
+router.put('/', async (req, res, next) => {
+  const id = req.body;
+  try {
+    const updatedLog = await LogEntry.findByIdAndUpdate(id, );
+  } catch (error) {
+  }
+})
+*/
 
 module.exports = router;
